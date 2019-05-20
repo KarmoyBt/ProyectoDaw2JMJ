1c82835692c9780ab89facb67fca5e39f47f7c3d
KarmoyBt <38316111+KarmoyBt@users.noreply.github.com>
2019-05-20 19:27:09 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -1,42 +1,33 @@
-<?php 
+<?php
+ 
+ public function getByLogin($login) {
+			$user = false;
+			if( $this->$_init) {
+				$res =  $this->$_db->prepare('SELECT * FROM `' .  $this->$_definition['table'] . '` WHERE `' .  $this->$_definition['login'] . '` = :login;');
+				$res->setFetchMode(\PDO::FETCH_ASSOC);
+				$res->execute(array('login' => $login));
+				if($lot = $res->fetch()) {
+					/* main fields, except password */
+					$user = array(
+						'id' => $lot[ $this->$_definition['id']],
+						'login' => $lot[ $this->$_definition['login']],
+						'key' =>  $lot[ $this->$_definition['key']],
+					);
+					/* other fields */
+					if(!empty( $this->$_definition['fields'])) {
+						foreach( $this->$_definition['fields'] as $field) {
+							$user[$field] = !empty($lot[$field]) ? $lot[$field] : '';
+						}
+					}
+				}
+			}
+			return $user;
+		}
 
-if(isset($_POST['inputUser']) && isset($_POST['inputContraseña'])){
-
-    $user = $_POST['inputUser'];   
-    $contra = $_POST['inputContraseña'];
-
-    //echo $user.''.$password;
-
-    //meterle db
-    $connection = mysqli_connect("localhost", "javimart_json", "nombreapellido.", "javimart_m6");
-
-    $data = mysqli_query($connection, "SELECT 'uid' FROM users WHERE  username = '{$user}' AND  contrasenya = '{$contra}'");
-
-    $row_cnt = mysqli_num_rows($data);
-
-    if($row_cnt == 1){
-        $row = mysqli_fetch_array($data);
-        $id = $row['ID'];
-        session_start();
-        $_SESSION['user_id'] = $id;
-        echo "success";
-
-    }else{
-        echo "No se ha encontrado datos";
-    }
-
-
-
-
-
-}
-if(isset($_POST['inputUser'])){
-    echo "Falta Usuario";
-}
-if(isset($_POST['inputContraseña'])){
-    echo "Falta contraseña";
-}
-
-
+public function logout() {
+			$_SESSION['user'] = null;
+			session_regenerate_id();
+			return true;
+		}
 
 ?>
