c294f4ed641f7b8947af52de5610f9ad2e087b62
KarmoyBt <38316111+KarmoyBt@users.noreply.github.com>
2019-05-27 16:49:56 +0200

    Add files via upload

--- class.test.php
+++ class.test.php
@@ -1,45 +1,22 @@
 <?php
 
+ class titulo {
 
-$id=$_POST["id"];
-$que=$_POST["que"];
+public $id= $_POST["id"];
+public $que= $_POST["que"];
 
-public function titulo($id,$que)
-{
+public function titulo($id,$que){
 
 
-$conexion = mysql_connect('localhost', 'usuario', 'contraseña') or die('Could not connect: ' . mysql_error());
+$conexion = mysql_connect('localhost', 'javimart_m6', 'nombreapellido.') or die('Could not connect: ' . mysql_error());
+
 mysql_select_db('bd', $conexion);
-$sql = mysql_query("SELECT * from usuarios");
+$sql = mysql_query("SELECT $que from Prueba WHERE id==$id");
 while($columna = mysql_fetch_array($sql)){
 	echo $columna["Nombre"];
 }
-	return $columna["Nombre"]
+	return $columna["Nombre"];
 }
-
-
-public function hash($id)
-{
-
-try{
-	$conn = new PDO('mysql:host=localhost;dbname=basededatos', $usuario, $contra);
-
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-	$sql = $conn->prepare('SELECT titulo FROM usuarios WHERE nombre = :Nombre');
-	$sql->execute(array('Nombre' => $id));
-	$resultado = $sql->fetchAll();
-
-	foreach ($resultado as $row) {
-		echo $row["Id"];
-	}
-}catch(PDOException $e){
-	echo "ERROR: " . $e->getMessage();
+    
 }
-
-
-return $row["Id"];
-
-}
-
 ?>
